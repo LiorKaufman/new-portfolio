05a152af4bd99e825bdfcebbf8e8a8ca5650d18b
Lior Kaufman <liorka1990@gmail.com>
2020-02-10 15:45:51 -0800

    h-test 1

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -6,7 +6,7 @@ class Portfolio extends Component {
   render() {
     if (this.props.data) {
       var projects = this.props.data.projects.map(function(projects) {
-        var projectImage = "/images/portfolio/" + projects.image;
+        var projectImage = "images/portfolio/" + projects.image;
         return (
 
               <div key={projects.title} className="card" style={{minWidth: "200px", width: "20rem", height: "auto", minHeight:"250px", margin: "2rem"}}>
