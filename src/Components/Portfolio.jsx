980c31d432126f59c39f51a352bcfcf7d513bd12
Lior Kaufman <liorka1990@gmail.com>
2020-03-11 15:08:46 -0700

    add new project

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,35 +1,45 @@
 import React, { Component } from "react";
 
 class Portfolio extends Component {
-
-
   render() {
     if (this.props.data) {
-      const imgUrls = ["images/portfolio/rsz_game-of-life.png","images/portfolio/glass.png","images/portfolio/location-list.png","images/portfolio/tic-tac-toe.png","images/portfolio/pig-latin.png"]
+      const imgUrls = [
+        "images/portfolio/rsz_game-of-life.png",
+        "images/portfolio/glass.png",
+        "images/portfolio/location-list.png",
+        "images/portfolio/tic-tac-toe.png",
+        "images/portfolio/pig-latin.png",
+        "images/portfolio/shoppinglist.png"
+      ];
       var projects = this.props.data.projects.map(function(projects, i) {
         var projectImage = "public/images/portfolio/" + projects.image;
         return (
-
-              <div key={projects.title} className="card" style={{minWidth: "200px", width: "20rem", height: "auto", minHeight:"250px", margin: "2rem"}}>
-                <a href={projects.url} title={projects.title} >
-                  <div className="card-header">
-                    <h5 className="card-title text-center">{projects.title}</h5>
-
-                  </div>
-                  <img src={imgUrls[i]} className="card-img-top border" alt="..."/>
-
-                  <div className="card-body">
-                    <p className="card-text">{projects.category}</p>
-                    <div className="card-footer text-muted">
-                      {projects.tools}
-                    </div>
-                  </div>
-                </a>
+          <div
+            key={projects.title}
+            className="card"
+            style={{
+              minWidth: "200px",
+              width: "20rem",
+              height: "auto",
+              minHeight: "250px",
+              margin: "2rem"
+            }}
+          >
+            <a href={projects.url} title={projects.title}>
+              <div className="card-header">
+                <h5 className="card-title text-center">{projects.title}</h5>
               </div>
+              <img src={imgUrls[i]} className="card-img-top border" alt="..." />
+
+              <div className="card-body">
+                <p className="card-text">{projects.category}</p>
+                <div className="card-footer text-muted">{projects.tools}</div>
+              </div>
+            </a>
+          </div>
         );
       });
     }
-
 
     return (
       <section id="portfolio">
@@ -39,7 +49,7 @@ class Portfolio extends Component {
 
             <div
               id="portfolio-wrapper"
-              className= "d-flex justify-content-between flex-wrap"
+              className="d-flex justify-content-between flex-wrap"
             >
               {projects}
             </div>
