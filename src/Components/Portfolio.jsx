d093a13a8afef1da781ab31049a85f832431e364
Lior Kaufman <liorka1990@gmail.com>
2020-03-17 11:13:28 -0700

    fix image

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,39 +1,39 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Portfolio extends Component {
   render() {
     if (this.props.data) {
       const imgUrls = [
-        "images/portfolio/rsz_game-of-life.png",
-        "images/portfolio/glass.png",
-        "images/portfolio/location-list.png",
-        "images/portfolio/tic-tac-toe.png",
-        "images/portfolio/pig-latin.png",
-        "images/portfolio/shoppinglist.png"
+        'images/portfolio/rsz_game-of-life.png',
+        'images/portfolio/glass.png',
+        'images/portfolio/location-list.png',
+        'images/portfolio/tic-tac-toe.png',
+        'images/portfolio/pig-latin.png',
+        'images/portfolio/shoppinglist.PNG'
       ];
       var projects = this.props.data.projects.map(function(projects, i) {
-        var projectImage = "public/images/portfolio/" + projects.image;
+        var projectImage = 'public/images/portfolio/' + projects.image;
         return (
           <div
             key={projects.title}
-            className="card"
+            className='card'
             style={{
-              minWidth: "200px",
-              width: "20rem",
-              height: "auto",
-              minHeight: "250px",
-              margin: "2rem"
+              minWidth: '200px',
+              width: '20rem',
+              height: 'auto',
+              minHeight: '250px',
+              margin: '2rem'
             }}
           >
             <a href={projects.url} title={projects.title}>
-              <div className="card-header">
-                <h5 className="card-title text-center">{projects.title}</h5>
+              <div className='card-header'>
+                <h5 className='card-title text-center'>{projects.title}</h5>
               </div>
-              <img src={imgUrls[i]} className="card-img-top border" alt="..." />
+              <img src={imgUrls[i]} className='card-img-top border' alt='...' />
 
-              <div className="card-body">
-                <p className="card-text">{projects.category}</p>
-                <div className="card-footer text-muted">{projects.tools}</div>
+              <div className='card-body'>
+                <p className='card-text'>{projects.category}</p>
+                <div className='card-footer text-muted'>{projects.tools}</div>
               </div>
             </a>
           </div>
@@ -42,14 +42,14 @@ class Portfolio extends Component {
     }
 
     return (
-      <section id="portfolio">
-        <div className="row">
-          <div className="twelve columns collapsed">
+      <section id='portfolio'>
+        <div className='row'>
+          <div className='twelve columns collapsed'>
             <h1>Check Out Some of My Works.</h1>
 
             <div
-              id="portfolio-wrapper"
-              className="d-flex justify-content-between flex-wrap"
+              id='portfolio-wrapper'
+              className='d-flex justify-content-between flex-wrap'
             >
               {projects}
             </div>
